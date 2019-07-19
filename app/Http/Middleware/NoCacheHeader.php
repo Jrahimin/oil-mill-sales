6622a86a0ad268b5e9d1eff2df7d14ad4260e38c
Junayed Rahimin <rahiminmyself@gmail.com>
2019-07-19 19:05:02 +0600

    no cache header middleware

--- app/Http/Middleware/NoCacheHeader.php
+++ app/Http/Middleware/NoCacheHeader.php
@@ -16,7 +16,10 @@ class NoCacheHeader
     public function handle($request, Closure $next)
     {
         $response = $next($request);
-        $response->header('Cache-Control', 'no-cache, must-revalidate');
+
+        $response->headers->set('Cache-Control', 'nocache, no-store, max-age=0, must-revalidate');
+        $response->headers->set('Pragma','no-cache');
+        $response->headers->set('Expires','Fri, 01 Jan 1990 00:00:00 GMT');
 
         return $response;
     }
